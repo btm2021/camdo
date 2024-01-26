58987daf5f5238e56b1f4f87ae9bdde1437b6c71
btm2021 <trinhminhbao@gmail.com>
2024-01-26 17:46:28 +0700

    Asd

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('express')();
-const { v4 } = require('uuid');
-
-app.get('/api', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`Hello! Go to item`);
-});
-
-module.exports = app;
